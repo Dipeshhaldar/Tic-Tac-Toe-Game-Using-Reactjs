5b2bcae7c386f28b2230e5efb26e5e39c367efb6
unknown <dipeshh.cse21@sbjit.edu.in>
2023-12-06 16:19:31 +0530

    State Updating Function added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,34 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+
+      if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+
+      return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+  //const [activePlayer, setActivePlayer] = useState("X");
 
-  function handleSelectSquare() {
-    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    //setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
@@ -29,10 +51,10 @@ function App() {
           </ol>
           <GameBoard
             onSelectSquare={handleSelectSquare}
-            activePlayerSymbol={activePlayer}
+            turns={gameTurns}
           />
         </div>
-        <Log />
+        <Log turns={gameTurns} />
       </main>
     </>
   );
